e3cdd26943ea8dc31f9abe13be5d2cc5d0cd061a
DenisKrivonosov <krivonosovdenis@gmail.com>
2016-02-19 22:16:25 +0300

    some changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// document.getElementById('search-opener').onclick = function() {
+//    popup =document.getElementById('search-popup'); 
+//   if(hasClass(popup, 'main-search-popup')) {
+//     popup.classList.add("fadeout");
+//   }
+//   else {
+//     console.log(2);
+//   }
+//   popup.classList.add('popup-animate');
+// }
+
 function init() {
   var mapDiv = document.getElementById('main_google_map');
     var map = new google.maps.Map(mapDiv, {
@@ -5,3 +16,13 @@ function init() {
       zoom: 9
     });
 }
+// hasClass = function(element, selector) {
+//   classList = element.classList;
+//     for (var i = 0, l = classList.length; i < l; i++) {
+//         if (classList[i] === selector) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
