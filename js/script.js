072118fb9980996b177a7d1ba640b93cf6126783
DenisKrivonosov <krivonosovdenis@gmail.com>
2016-02-22 23:31:21 +0300

    first try changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-document.getElementById('search-opener').onclick = function(event) {
-    event.preventDefault();
-    console.log(123);
-    popup =document.getElementById('search-popup');
-  if(hasClass(popup, 'open')) {
-     popup.classList.remove("open");
+document.getElementById("search-opener").onclick = function(event) {
+  event.preventDefault();
+  console.log(123);
+  popup =document.getElementById("search-popup");
+  if(hasClass(popup, "open")) {
+    popup.classList.remove("open");
   }
   else {
     popup.classList.add("open");
@@ -11,7 +11,7 @@ document.getElementById('search-opener').onclick = function(event) {
 }
 
 function init() {
-  var mapDiv = document.getElementById('main_google_map');
+  var mapDiv = document.getElementById("main_google_map");
   var SedonaLatLng = {lat: 34.878397, lng:-111.762395};
   var map = new google.maps.Map(mapDiv, {
     center: {lat: 34.757071, lng: -111.736274},
@@ -20,16 +20,16 @@ function init() {
   var marker = new google.maps.Marker({
   position: SedonaLatLng,
   map: map,
-  title: 'Sedona dream'
+  title: "Sedona dream"
   });
 }
+
 function  hasClass(element, selector) {
   classList = element.classList;
     for (var i = 0, l = classList.length; i < l; i++) {
-        if (classList[i] === selector) {
-            return true;
-        }
+      if (classList[i] === selector) {
+        return true;
+      }
     }
-
     return false;
 }
